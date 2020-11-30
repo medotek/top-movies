9c1ff2580c3f07dab72b8f48004f5d7ad9067658
Thomas Huynh <tomsyan03@gmail.com>
2020-11-30 21:41:18 +0100

    final

--- src/components/Films/index.js
+++ src/components/Films/index.js
@@ -6,14 +6,23 @@ import Col from 'react-bootstrap/Col';
 export function Films(props) {
     return (
 
-        <Col lg="2" xs="2" md="2">
+        <Col lg="2" xs="6" md="4">
             <div className="card">
                 <div className="content text-center">
+                    <div className="info">
+                        Année de sortie {props.year} <br />
+                        Production : {props.crew}<br />
+                        Vote : {props.imDbRating}/10
+                    </div>
                     <img
                         src={props.image}
                         alt={props.title}
                         style={{ width: "100%" }}
                     />
+
+                    <div className="rank">
+                        {props.rank}
+                    </div>
                     <div className="film-title">
                         <h4>{props.title}</h4>
                     </div>
