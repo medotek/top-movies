b38bdf19e0e3ce242c2eaf8ccf9394c03aa433b5
Thomas Huynh <tomsyan03@gmail.com>
2020-11-30 20:40:57 +0100

    dependencies update + bootstrap

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import "../../styles.css"
 import "./App.css";
 import films from "../../films.json";
-import { Films } from '../Films'
+import { Films } from '../Films';
+import  Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class App extends Component {
   constructor(props) {
@@ -12,13 +15,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <main className="main-area">
-          <div className="cards">
-            {films.items.map((film, index) => {
-              return <Films {...film} key={index} />;
-            })}
-          </div>
-        </main>
+        <Container>
+          <Row>
+            <Col>
+              <div className="cards">
+                {films.items.map((film, index) => {
+                  return <Films {...film} key={index} />;
+                })}
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
